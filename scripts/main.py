dc23fb4b1755c5b2bec393a322bfadd06b62930e
haeu07 <ralf.haeussler@sonymusic.com>
2021-06-16 18:11:38 +0200

    ...

--- scripts/main.py
+++ scripts/main.py
@@ -17,5 +17,26 @@ def test_db( p_trans_id ):
     print( f"""Hello DB: {a_row['trans_info_loc_key']}""")
 
 
+def test_db_oci( p_trans_id ):
+    t.logger( log_type=t.LOG_TYPE_MESSAGE, run_id=1, log_text=f'first test here' )
+    con = db.connect( run_id=p_trans_id, ini_file_section='dbotdsr2.rzsamgpp_int' )
+    cur = db.open_cursor( con, run_id=p_trans_id )
+
+
+    run_sql = f"""select pt.product_type_key, pt.product_type_name
+                from dig_product_type pt"""
+    db.execute(cur, run_sql, run_id=p_trans_id)
+    #cur.rowfactory = lambda *args: dict(zip([d[0] for d in cur.description], args))
+
+    #a_row = db.fetchone(cur)
+    all_rows = db.fetchall(cur)
+
+    print( f"""Hello Oracle DB""")
+
+    for one_row in all_rows:
+        print( one_row['PRODUCT_TYPE_KEY'] + " " + one_row['PRODUCT_TYPE_NAME'] )
+
+
 if __name__ == '__main__':
-    test_db( p_trans_id = 544 )
+    #test_db( p_trans_id = 544 )
+    test_db_oci( p_trans_id = 544 )
