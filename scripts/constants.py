8977012c03ab3c8a2355c4b38a32e35b8610b229
haeu07 <ralf.haeussler@sonymusic.com>
2021-07-20 15:30:38 +0200

    test script to evalueate access

--- scripts/constants.py
+++ scripts/constants.py
@@ -13,19 +13,20 @@ PP_DEFAULT_TIMESTAMP_FMT = 'yyyy-MM-dd HH:mm:ss'
 PP_DEFAULT_TIMESTAMP_FMT_PY = '%Y%m%d %H:%M:%S'
 PP_DEFAULT_DATE_FMT = 'yyyyMMdd'
 
-#if platform == 'win32':
-PP_DB_DEFAULT_INI_FILE_SECTION = 'dbotdsr2.rzsamgpp_int'
-PP_ROOT = os.path.normpath( PP_ROOT )
-PP_SCRIPTS_SUB_PATH = os.path.normpath('OCI_AWS_INTERFACE\\scripts')
-PP_LOG_SUB_PATH = os.path.normpath('OCI_AWS_INTERFACE\\log')
-PP_DATA_SUB_PATH = os.path.normpath('OCI_AWS_INTERFACE\\local_data')
+if platform == 'win32':
+    PP_DB_DEFAULT_INI_FILE_SECTION = 'dbotdsr2.rzsamgpp_int'
+    PP_ROOT = os.path.normpath( PP_ROOT )
+    PP_SCRIPTS_SUB_PATH = 'OCI_AWS_INTERFACE\\scripts'
+    PP_LOG_SUB_PATH = 'OCI_AWS_INTERFACE\\log'
+    PP_DATA_SUB_PATH = 'OCI_AWS_INTERFACE\\local_data'
 
-#else:
-#    PP_DB_DEFAULT_INI_FILE_SECTION = 'dbotdsr2.rzsamgpp_int'
-#    PP_ROOT = os.path.normpath( "/home/hadoop" )
-#    PP_SCRIPTS_SUB_PATH = 'OCI_AWS_INTERFACE/scripts'
-#    PP_LOG_SUB_PATH = 'OCI_AWS_INTERFACE/log'
-#    PP_DATA_SUB_PATH = 'OCI_AWS_INTERFACE/local_data'
+else:
+    PP_DB_DEFAULT_INI_FILE_SECTION = 'dbotdsr2.rzsamgpp_int'
+    # PP_ROOT = os.path.normpath( "/home/hadoop" )
+    PP_ROOT = os.path.normpath( PP_ROOT )
+    PP_SCRIPTS_SUB_PATH = 'OCI_AWS_INTERFACE/scripts'
+    PP_LOG_SUB_PATH = 'OCI_AWS_INTERFACE/log'
+    PP_DATA_SUB_PATH = 'OCI_AWS_INTERFACE/local_data'
 
 PP_SCRIPTS  = os.path.join( PP_ROOT, PP_SCRIPTS_SUB_PATH )
 PP_LOG      = os.path.join( PP_ROOT, PP_LOG_SUB_PATH )
