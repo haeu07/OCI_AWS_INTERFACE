e64b3d9a14e75354a39dc92b4f2a28a53dd35e44
haeu07 <ralf.haeussler@sonymusic.com>
2021-07-20 15:33:47 +0200

    test script to evalueate access

--- scripts/constants.py
+++ scripts/constants.py
@@ -9,6 +9,7 @@ PP_DB_DEFAULT_INI_FILE_SECTION = 'dbotdsr2.rzsamgpp_int'
 
 PP_ROOT = str(pathlib.Path(__file__).parent.absolute())
 PP_ROOT = PP_ROOT.replace('\OCI_AWS_INTERFACE\scripts','')
+PP_ROOT = PP_ROOT.replace('/OCI_AWS_INTERFACE/scripts','')
 PP_DEFAULT_TIMESTAMP_FMT = 'yyyy-MM-dd HH:mm:ss'
 PP_DEFAULT_TIMESTAMP_FMT_PY = '%Y%m%d %H:%M:%S'
 PP_DEFAULT_DATE_FMT = 'yyyyMMdd'
